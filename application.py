d56c552e7d7a2e7ef7cf226c683ca615e2ed5741
Joshua Drummond Kaunert <socialsketch@icloud.com>
2018-08-03 15:37:57 +0000

    search functionality added

--- application.py
+++ application.py
@@ -25,28 +25,14 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def index():
+    try:
+        if session['logged_in'] :
+            return redirect(url_for('search', username=session['username']))
+
+    except KeyError:
+        return render_template("index.html")
+
     return render_template("index.html")
-
-@app.route('/signin', methods = ["GET","POST"])
-def signin():
-
-    if request.method == "POST":
-
-        session['username'] = request.form.get("username")
-        session['password'] = request.form.get("password")
-
-        if session['username'] != "" and session['password'] != "":
-            if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username":session['username'], "password":session['password']}).rowcount == 0:
-                return render_template("error.html", message="No such user registered.")
-
-        else:
-            return render_template("error.html", message="please try again.")
-
-        #return profile(username)
-        return redirect(url_for("profile", username=session['username']))
-
-
-    return render_template("signin.html")
 
 @app.route('/signup', methods = ["GET","POST"])
 def signup():
@@ -64,24 +50,56 @@ def signup():
 
     return render_template("signup.html")
 
+@app.route('/signin', methods = ["GET","POST"])
+def signin():
+
+    if request.method == "POST":
+
+        session['username'] = request.form.get("username")
+        session['password'] = request.form.get("password")
+        session['logged_in'] = False
+
+        if session['username'] != "" and session['password'] != "":
+            if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username":session['username'], "password":session['password']}).rowcount == 0:
+                return render_template("error.html", message="No such user registered.")
+
+        else:
+            return render_template("error.html", message="please try again.")
+
+        #return profile(username)
+        session['logged_in'] = True
+        return redirect(url_for("search", username=session['username']))
 
 
-@app.route('/user/<username>')
-def profile(username):
-    return render_template("profile.html", username=username)
+    return render_template("signin.html")
 
-@app.route('/user/<username>/location/<location>')
-def location(username, location):
+
+@app.route('/search')
+def search():
+    return render_template("search.html")
+
+@app.route('/location/<location>')
+def location(location):
     return render_template("location.html", location=location)
 
-@app.route('/search-results', methods = ["GET", "POST"])
-def search():
 
-    #return redirect(url_for('search'))
-    return render_template("search-results.html", username=session['username'])
+@app.route('/search-results', methods = ["GET", "POST"])
+def searchResults():
+    if request.method=="POST":
+        session['searchterm'] = request.form.get('searchterm')
+
+        """Lists all matches."""
+        session['locations'] = db.execute("SELECT * FROM locations WHERE id < 20").fetchall()
+        return render_template("search-results.html", locations=session['locations'], searchterm=session['searchterm'])
+
+    return render_template("search-results.html", searchterm=session['searchterm'])
 
 @app.route('/logout')
 def logout():
     [session.pop(key) for key in list(session.keys()) if key != '_flashes']
     return "successfully logged out"
+
+@app.route('/api/<int:zipcode>')
+def api(zipcode):
+    return render_template("api.html", zipcode=zipcode)
 
